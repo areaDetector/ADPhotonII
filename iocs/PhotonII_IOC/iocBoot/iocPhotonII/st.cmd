437250cf74725238840ae1ce857e36170fcb129c
Mark Rivers <rivers@cars.uchicago.edu>
2017-06-11 17:54:10 -0500

    Many changes

--- iocs/PhotonII_IOC/iocBoot/iocPhotonII/st.cmd
+++ iocs/PhotonII_IOC/iocBoot/iocPhotonII/st.cmd
@@ -1,19 +1,19 @@
 < envPaths
 errlogInit(20000)
 
-dbLoadDatabase("$(TOP)/dbd/marCCDApp.dbd")
-marCCDApp_registerRecordDeviceDriver(pdbbase) 
+dbLoadDatabase("$(TOP)/dbd/PhotonIIApp.dbd")
+PhotonIIApp_registerRecordDeviceDriver(pdbbase) 
 
 # Prefix for all records
-epicsEnvSet("PREFIX", "13MARCCD1:")
+epicsEnvSet("PREFIX", "13PII_1:")
 # The port name for the detector
-epicsEnvSet("PORT",   "MAR")
+epicsEnvSet("PORT",   "PII")
 # The queue size for all plugins
 epicsEnvSet("QSIZE",  "20")
 # The maximim image width; used for row profiles in the NDPluginStats plugin
-epicsEnvSet("XSIZE",  "2048")
+epicsEnvSet("XSIZE",  "768")
 # The maximim image height; used for column profiles in the NDPluginStats plugin
-epicsEnvSet("YSIZE",  "2048")
+epicsEnvSet("YSIZE",  "1024")
 # The maximum number of time series points in the NDPluginStats plugin
 epicsEnvSet("NCHANS", "2048")
 # The maximum number of frames buffered in the NDPluginCircularBuff plugin
@@ -21,26 +21,29 @@ epicsEnvSet("CBUFFS", "500")
 # The search path for database files
 epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
 
-###
-# Create the asyn port to talk to the MAR on port 2222
-drvAsynIPPortConfigure("marServer","gse-marccd1.cars.aps.anl.gov:2222")
-# Set the input and output terminators.
-asynOctetSetInputEos("marServer", 0, "\n")
-asynOctetSetOutputEos("marServer", 0, "\n")
-#asynSetTraceMask("marServer",0,255)
-asynSetTraceIOMask("marServer",0,2)
+# The name of the drvAsynIPPort to communicate with p2util
+epicsEnvSet("PII_SERVER",   "PIIServer")
 
-marCCDConfig("$(PORT)", "marServer", 0, 0)
-dbLoadRecords("$(ADMARCCD)/db/marCCD.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1,MARSERVER_PORT=marServer")
+###
+# Create the asyn port to talk to the p2util procServ port 20000
+drvAsynIPPortConfigure("$(PII_SERVER)","localhost:20000")
+# Set the input and output terminators.
+asynOctetSetInputEos("$(PII_SERVER)", 0, "\r\n")
+asynOctetSetOutputEos("$(PII_SERVER)", 0, "\n")
+asynSetTraceMask("$(PII_SERVER)",0,9)
+asynSetTraceIOMask("$(PII_SERVER)",0,2)
+
+PhotonIIConfig("$(PORT)", "$(PII_SERVER)", 0, 0)
+dbLoadRecords("$(ADPHOTONII)/db/PhotonII.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1,PII_SERVER_PORT=$(PII_SERVER)")
 
 # Create a standard arrays plugin
 NDStdArraysConfigure("Image1", 5, 0, "$(PORT)", 0, 0)
 # Make NELEMENTS in the following be a little bigger than 2048*2048
-dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),TYPE=Int16,FTVL=SHORT,NELEMENTS=4200000")
+dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),TYPE=Int32,FTVL=LONG,NELEMENTS=786432")
 
 # Load all other plugins using commonPlugins.cmd
 < $(ADCORE)/iocBoot/commonPlugins.cmd
-set_requestfile_path("$(ADMARCCD)/marCCDApp/Db")
+set_requestfile_path("$(ADPHOTONII)/PhotonIIApp/Db")
 
 #asynSetTraceMask("$(PORT)",0,3)
 #asynSetTraceIOMask("$(PORT)",0,4)
